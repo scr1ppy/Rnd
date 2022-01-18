1ff0eb09bd0ffaa9a70cccb2bfb0191bf77c3880
group5 <vendserday@gmail.com>
2022-01-18 18:16:24 +0200

    -while-

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+    new Main().run();
+
+
+    }
+
+    private void run() {
+        Random random = new Random();
+        int[] mass = fillMass(20);
+        int j = 1;
+        int maxJ = mass[0];
+        while(j < mass.length){
+            if(mass[j]>maxJ) maxJ = mass[j];
+                j++;
+        }
+        System.out.println(maxJ);
+
+
+
+        }
+
+
+    private int[] fillMass(int n) {
+      int[] mass = new int[n];
+        Random random = new Random();
+        for (int i = 0; i < n; i++) {
+            mass[i] = random.nextInt(100);
+        }
+        return mass;
+    }
+}
